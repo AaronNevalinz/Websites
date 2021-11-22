304ebfdfbd19716072ad8f856a005b0d62a5d525
Aaron Nevalinz Ogwal <76948733+NeVaLinZ@users.noreply.github.com>
2021-11-22 12:06:27 +0300

    Add files via upload

---
+++ twitter clone site/Twitter clone site/twitter.js
@@ -0,0 +1,7 @@
+$(".dad").click(function(){
+    $("div").css("backgroundColor", "#000");
+    $("div").css("color", "#fff");
+    $("h3").css("color", "#fff");
+    $("button").css("backgroundColor", "#fff");
+    $("button").css("color", "#000");
+});
